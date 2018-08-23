b8fd72e6025af361dbb3c6b49762452df727ec8b
ericmck2000 <ericmck2000@gmail.com>
2018-08-23 15:20:21 -0500

    Added ClearMessages module. Will delete ALL messages from a channel.

---
+++ WarBot.Modules/GuildCommandModuleBase/ClearMessagesModule.cs
@@ -0,0 +1,64 @@
+using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using WarBot.Attributes;
+using WarBot.Core;
+using WarBot.Core.ModuleType;
+using System.Linq;
+using System;
+
+namespace WarBot.Modules.GuildCommandModules
+{
+    //Required chat context type.
+    [RequireContext(ContextType.Guild)]
+    public class ClearMessagesModule : GuildCommandModuleBase
+    {
+        [Command("clear messages"), Alias("clear")]
+        [RoleLevel(RoleLevel.Leader)]
+        [Summary("Delete specified messages from a channel.")]
+        [RequireBotPermission(ChannelPermission.ManageMessages)]
+        ///action
+        ///NonPinned = Only non-pinned messages
+        ///Pinned = Only Pinned Messages
+        ///ALL = Everything!
+        public async Task ClearMessages(string action = "NonPinned")
+        {
+            bool SelectPinned = !action.Equals("NonPinned", StringComparison.OrdinalIgnoreCase);
+
+            DateTimeOffset discordBulkCutoffDate = DateTimeOffset.Now.AddDays(-13);
+            while (true)
+            {
+                var x = this.Context.Channel.GetMessagesAsync(500);
+                IEnumerable<IMessage> results = await x.Flatten();
+
+                var matchingResults = results
+                    .Where(o => o.IsPinned == SelectPinned);
+
+                var ToBulkDelete = matchingResults
+                    .Where(o => o.CreatedAt > discordBulkCutoffDate);
+
+                try
+                {
+                    //If there are messages to bulk delete, do it.
+                    if (ToBulkDelete.Count() > 0)
+                        await Context.Channel.DeleteMessagesAsync(ToBulkDelete);
+                    //Once everything has been bulk deleted, start deleting one by one.
+                    else if (matchingResults.Count() > 0)
+                        foreach (IMessage msg in matchingResults)
+                            await msg.DeleteAsync();
+                    //If nothing else to delete, stop deleting stuff.
+                    else
+                        break;
+                }
+                catch(Exception ex)
+                {
+                    throw;
+                }
+            }
+
+        }
+    }
+}
