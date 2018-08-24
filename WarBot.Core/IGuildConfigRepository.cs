9b28b27f5e537c2c20780d6b58ced034c1d6ba6a
ericmck2000 <ericmck2000@gmail.com>
2018-08-24 15:18:25 -0500

    Needed a method to force load a guild, with a specified enviornment. Used for setting a guild's environment.

--- WarBot.Core/IGuildConfigRepository.cs
+++ WarBot.Core/IGuildConfigRepository.cs
@@ -7,5 +7,14 @@ namespace WarBot.Core
     public interface IGuildConfigRepository
     {
         Task<IGuildConfig> GetConfig(SocketGuild Guild);
+
+        /// <summary>
+        /// Force a config to load as the specified environment.
+        /// This should almost never be used.
+        /// </summary>
+        /// <param name="Guild"></param>
+        /// <param name="Environment"></param>
+        /// <returns></returns>
+        Task<IGuildConfig> GetConfig(SocketGuild Guild, Environment? Environment);
     }
 }
