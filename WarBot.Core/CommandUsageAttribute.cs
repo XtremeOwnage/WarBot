f8cddc6c082071dc47b50d890b373578c8ac0e34
ericmck2000 <ericmck2000@gmail.com>
2018-08-27 15:54:04 -0500

    Added attribute to show command usage examples.

---
+++ WarBot.Core/CommandUsageAttribute.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace WarBot.Core
+{
+    /// <summary>
+    /// Gives a usage example.
+    /// </summary>
+    public class CommandUsageAttribute : Attribute
+    {
+        public string Usage { get; }
+
+        public CommandUsageAttribute(string Usage)
+        {
+            this.Usage = Usage;
+        }
+    }
+}
