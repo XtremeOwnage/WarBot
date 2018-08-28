12655dcc442441e3f69c5c8fcd93d59ddccf8303
ericmck2000 <ericmck2000@gmail.com>
2018-08-28 15:13:14 -0500

    Added a .js file, to launch warbot from PM2.

---
+++ WarBot.Main/Launch.js
@@ -0,0 +1,10 @@
+﻿var exec = require('child_process').execFile;
+
+var fun = function () {
+    console.log("fun() start");
+    exec('HelloJithin.exe', function (err, data) {
+        console.log(err)
+        console.log(data.toString());
+    });
+}
+fun();
