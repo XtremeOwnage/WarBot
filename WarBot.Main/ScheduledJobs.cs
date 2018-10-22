d79ba764ff04293ee6acc4b2edfbcb40ec6956a5
ericmck2000 <ericmck2000@gmail.com>
2018-10-22 15:19:58 -0500

    Wrapped a try/catch statement, to assist in setting up quartz properly.

--- WarBot.Main/ScheduledJobs.cs
+++ WarBot.Main/ScheduledJobs.cs
@@ -1,4 +1,5 @@
-﻿using WarBot.Core.JobScheduling;
+﻿using System;
+using WarBot.Core.JobScheduling;
 using WarBot.Util;
 
 namespace WarBot
@@ -11,20 +12,27 @@ namespace WarBot
         /// <param name="job"></param>
         public static void ScheduleJobs(IJobScheduler job)
         {
-            job.RecurringJob<WAR_Messages>("war1_prep_started", o => o.SendWarPrepStarted(1), "0 2 * * *");
-            job.RecurringJob<WAR_Messages>("war2_prep_started", o => o.SendWarPrepStarted(2), "0 8 * * *");
-            job.RecurringJob<WAR_Messages>("war3_prep_started", o => o.SendWarPrepStarted(3), "0 14 * * *");
-            job.RecurringJob<WAR_Messages>("war4_prep_started", o => o.SendWarPrepStarted(4), "0 20 * * *");
+            try
+            {
+                job.RecurringJob<WAR_Messages>("war1_prep_started", o => o.SendWarPrepStarted(1), "0 2 * * *");
+                job.RecurringJob<WAR_Messages>("war2_prep_started", o => o.SendWarPrepStarted(2), "0 8 * * *");
+                job.RecurringJob<WAR_Messages>("war3_prep_started", o => o.SendWarPrepStarted(3), "0 14 * * *");
+                job.RecurringJob<WAR_Messages>("war4_prep_started", o => o.SendWarPrepStarted(4), "0 20 * * *");
 
-            job.RecurringJob<WAR_Messages>("war1_prep_ending", o => o.SendWarPrepEnding(1), "45 3 * * *");
-            job.RecurringJob<WAR_Messages>("war2_prep_ending", o => o.SendWarPrepEnding(2), "45 9 * * *");
-            job.RecurringJob<WAR_Messages>("war3_prep_ending", o => o.SendWarPrepEnding(3), "45 15 * * *");
-            job.RecurringJob<WAR_Messages>("war4_prep_ending", o => o.SendWarPrepEnding(4), "45 21 * * *");
+                job.RecurringJob<WAR_Messages>("war1_prep_ending", o => o.SendWarPrepEnding(1), "45 3 * * *");
+                job.RecurringJob<WAR_Messages>("war2_prep_ending", o => o.SendWarPrepEnding(2), "45 9 * * *");
+                job.RecurringJob<WAR_Messages>("war3_prep_ending", o => o.SendWarPrepEnding(3), "45 15 * * *");
+                job.RecurringJob<WAR_Messages>("war4_prep_ending", o => o.SendWarPrepEnding(4), "45 21 * * *");
 
-            job.RecurringJob<WAR_Messages>("war1_started", o => o.SendWarStarted(1), "0 4 * * *");
-            job.RecurringJob<WAR_Messages>("war2_started", o => o.SendWarStarted(2), "0 10 * * *");
-            job.RecurringJob<WAR_Messages>("war3_started", o => o.SendWarStarted(3), "0 16 * * *");
-            job.RecurringJob<WAR_Messages>("war4_started", o => o.SendWarStarted(4), "0 22 * * *");
+                job.RecurringJob<WAR_Messages>("war1_started", o => o.SendWarStarted(1), "0 4 * * *");
+                job.RecurringJob<WAR_Messages>("war2_started", o => o.SendWarStarted(2), "0 10 * * *");
+                job.RecurringJob<WAR_Messages>("war3_started", o => o.SendWarStarted(3), "0 16 * * *");
+                job.RecurringJob<WAR_Messages>("war4_started", o => o.SendWarStarted(4), "0 22 * * *");
+            }
+            catch(Exception ex)
+            {
+                throw ex;
+            }
         }
     }
 }
