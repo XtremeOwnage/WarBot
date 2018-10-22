33fd0bc7c1b05552b77074e04bfdda3602c0162a
ericmck2000 <ericmck2000@gmail.com>
2018-10-22 10:55:50 -0500

    Commented out a lot of voting-related code.... since, a lot of changes were lost relating to votes.

--- WarBot.Main/WARBOT_Votes.cs
+++ WarBot.Main/WARBOT_Votes.cs
@@ -1,13 +1,11 @@
-﻿using System;
+﻿using Discord;
+using Discord.WebSocket;
+using System;
 using System.Collections.Concurrent;
 using System.Threading.Tasks;
-using WarBot.Core.Dialogs;
-using System.Linq;
-using WarBot.Core.Voting;
-using WarBot.Modules.GuildCommandModules;
-using Discord;
-using Discord.WebSocket;
 using WarBot.Core;
+using WarBot.Storage.Models.Voting;
+using System.Linq;
 
 namespace WarBot
 {
@@ -52,73 +50,75 @@ namespace WarBot
 
         private async Task Client_ReactionRemoved(Cacheable<IUserMessage, ulong> arg1, ISocketMessageChannel arg2, SocketReaction arg3)
         {
-            try
-            {
-                if (!ActivePolls.ContainsKey(arg1.Id))
-                    return;
-                //Ignore if this was a reaction I added.
-                else if (arg3.UserId != Client.CurrentUser.Id)
-                    return;
+            //try
+            //{
+            //    if (!ActivePolls.ContainsKey(arg1.Id))
+            //        return;
+            //    //Ignore if this was a reaction I added.
+            //    else if (arg3.UserId != Client.CurrentUser.Id)
+            //        return;
 
-                var CurrentPoll = ActivePolls[arg1.Id];
+            //    var CurrentPoll = ActivePolls[arg1.Id];
 
-                if (CurrentPoll.EmojiOption.ContainsKey(arg3.Emote))
-                {
-                    await arg2.SendMessageAsync($"Note- One of the options for the pool was removed by {arg3.User.Value.Mention}. I have added the reaction back.");
-                    await CurrentPoll.Message.AddReactionAsync(arg3.Emote);
-                }
-                else
-                {
-                    //The reaction removed was not apart of the poll.
-                }
-            }
-            catch (Exception ex)
-            {
-                await Log.Error(null, ex);
-            }
+            //    if (CurrentPoll.Options.Any(o => o.Emote == arg3.Emote))
+            //    {
+            //        await arg2.SendMessageAsync($"Note- One of the options for the pool was removed by {arg3.User.Value.Mention}. I have added the reaction back.");
+            //        await CurrentPoll.Message.AddReactionAsync(arg3.Emote);
+            //    }
+            //    else
+            //    {
+            //        //The reaction removed was not apart of the poll.
+            //    }
+            //}
+            //catch (Exception ex)
+            //{
+            //    await Log.Error(null, ex);
+            //}
         }
 
         private async Task Client_ReactionAdded(Cacheable<IUserMessage, ulong> arg1, ISocketMessageChannel arg2, SocketReaction arg3)
         {
-            try
-            {
-                if (!ActivePolls.ContainsKey(arg1.Id))
-                    return;
-                //Ignore if I(Warbot) added the reaction.
-                else if (arg3.UserId == Client.CurrentUser.Id)
-                    return;
+            //try
+            //{
+            //    if (!ActivePolls.ContainsKey(arg1.Id))
+            //        return;
+            //    //Ignore if I(Warbot) added the reaction.
+            //    else if (arg3.UserId == Client.CurrentUser.Id)
+            //        return;
 
-                var CurrentPoll = ActivePolls[arg1.Id];
-                if (CurrentPoll.EmojiOption.ContainsKey(arg3.Emote))
-                {
-                    //Test if the user has already voted.
-                    if (CurrentPoll.UserVotes.ContainsKey(arg3.UserId))
-                    {
-                        var ToRemove = CurrentPoll.UserVotes[arg3.UserId];
-                        //Remove the reaction.
-                        await CurrentPoll.Message.RemoveReactionAsync(ToRemove, arg3.User.Value);
+            //    var CurrentPoll = ActivePolls[arg1.Id];
+            //    if (CurrentPoll.Options.Any(o => o.Emote == arg3.Emote))
+            //    {
+            //        //Test if the user has already voted.
+            //        if (CurrentPoll.Votes.Any(o => o.UserId == arg3.UserId))
+            //        {
+            //            var ToRemove = CurrentPoll.Votes.Where(o => o.UserId == arg3.UserId);
+            //            //Remove the reaction.
+            //            await CurrentPoll.Message.RemoveReactionAsync(ToRemove, arg3.User.Value);
 
-                        //Update their vote.
-                        CurrentPoll.UserVotes[arg3.UserId] = arg3.Emote;
-                    }
-                    else
-                    {
-                        //Somebody is voting in the poll.
-                        CurrentPoll.UserVotes.TryAdd(arg3.UserId, arg3.Emote);
-                    }
+            //            //Update their vote.
+            //            //urrentPoll.Votes.Add(new UserVote(arg3, [arg3.UserId] = arg3.Emote;
 
-                }
-                else
-                {
-                    //Somebody added a new reaction.
-                    await arg2.SendMessageAsync($"{arg3.User.Value.Mention}, Please do not add new reactions to my poll.");
-                    await CurrentPoll.Message.RemoveReactionAsync(arg3.Emote, arg3.User.Value);
-                }
-            }
-            catch (Exception ex)
-            {
-                await Log.Error(null, ex);
-            }
+            //            //TODO - Correct the above line.
+            //        }
+            //        else
+            //        {
+            //            //Somebody is voting in the poll.
+            //            //CurrentPoll.UserVotes.TryAdd(arg3.UserId, arg3.Emote);
+            //        }
+
+            //    }
+            //    else
+            //    {
+            //        //Somebody added a new reaction.
+            //        await arg2.SendMessageAsync($"{arg3.User.Value.Mention}, Please do not add new reactions to my poll.");
+            //        await CurrentPoll.Message.RemoveReactionAsync(arg3.Emote, arg3.User.Value);
+            //    }
+            //}
+            //catch (Exception ex)
+            //{
+            //    await Log.Error(null, ex);
+            //}
         }
     }
 }
